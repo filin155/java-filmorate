9394937cf4f9d84bd5d9278c96dc73ed1923b410
Manzar <artem.manzar@yandex.ru>
2024-03-12 08:54:00 +0300

    add annotation validate for model

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -5,13 +5,22 @@ import lombok.Data;
 
 import java.time.LocalDate;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+
 @Data
 @Builder
 public class Film {
     private int id;
+    @NotNull
+    @NotBlank
     private String name;
+    @Size(max = 200)
     private String description;
     private LocalDate releaseDate;
+    @Positive
     private int duration;
 
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -5,14 +5,27 @@ import lombok.Data;
 
 import java.time.LocalDate;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.Pattern;
+
 @Data
 @Builder
 public class User {
 
     private int id;
+    @NotNull
+    @NotBlank
+    @Email
     private String email;
+    @NotNull
+    @NotBlank
+    @Pattern(regexp = "^((?! ).)*$")
     private String login;
     private String name;
+    @PastOrPresent
     private LocalDate birthday;
 
 
