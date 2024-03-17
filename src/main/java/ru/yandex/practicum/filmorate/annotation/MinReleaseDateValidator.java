0759274961020dba6a97af433292255cba25ea1e
Manzar <artem.manzar@yandex.ru>
2024-03-17 21:07:41 +0300

    fix checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/annotation/MinReleaseDateValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/annotation/MinReleaseDateValidator.java
@@ -7,10 +7,12 @@ import javax.validation.ConstraintValidatorContext;
 
 public class MinReleaseDateValidator implements ConstraintValidator<MinReleaseDate, LocalDate> {
     private LocalDate minimumDate;
+
     @Override
     public void initialize(MinReleaseDate constraintAnnotation) {
         minimumDate = LocalDate.parse(constraintAnnotation.value());
     }
+
     @Override
     public boolean isValid(LocalDate value, ConstraintValidatorContext context) {
         return value == null || !value.isBefore(minimumDate);
