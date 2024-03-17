c69802f1a15f4a52c3cdbb5bf50ed5457c900c50
Manzar <artem.manzar@yandex.ru>
2024-03-17 21:05:50 +0300

    fix checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/annotation/MinReleaseDate.java
+++ src/main/java/ru/yandex/practicum/filmorate/annotation/MinReleaseDate.java
@@ -6,11 +6,11 @@ import java.lang.annotation.RetentionPolicy;
 import javax.validation.Constraint;
 import javax.validation.constraints.Past;
 
-
 @Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = MinReleaseDateValidator.class)
 @Past
 public @interface MinReleaseDate {
+
     String message() default "Date must not be before {value}";
 
     Class<?>[] groups() default {};
